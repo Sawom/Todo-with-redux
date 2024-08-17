b9a0c5a0e0a53a29884663889465a32b37842b5b
Sawom <asawom250@gmail.com>
2024-08-18 02:55:57 +0600

    update

--- src/components/todo/AddTodoModal.tsx
+++ src/components/todo/AddTodoModal.tsx
@@ -12,6 +12,7 @@ import { Input } from "../ui/input";
 import { Label } from "../ui/label";
 
 const AddTodoModal = () => {
+  
   return (
     <Dialog>
       <DialogTrigger asChild>
@@ -20,27 +21,25 @@ const AddTodoModal = () => {
       <DialogContent className="sm:max-w-[425px]">
         <DialogHeader>
           <DialogTitle>Edit profile</DialogTitle>
-          <DialogDescription>
-            Make changes to your profile here. Click save when you're done.
-          </DialogDescription>
+          <DialogDescription>Add your task</DialogDescription>
         </DialogHeader>
         <div className="grid gap-4 py-4">
           <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="name" className="text-right">
-              Name
+            <Label htmlFor="task" className="text-right">
+              Task
             </Label>
             <Input
-              id="name"
+              id="task"
               defaultValue="Pedro Duarte"
               className="col-span-3"
             />
           </div>
           <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="username" className="text-right">
-              Username
+            <Label htmlFor="description" className="text-right">
+              Description
             </Label>
             <Input
-              id="username"
+              id="description"
               defaultValue="@peduarte"
               className="col-span-3"
             />
--- src/components/todo/TodoContainer.tsx
+++ src/components/todo/TodoContainer.tsx
@@ -1,13 +1,14 @@
 import { Button } from "../ui/button";
 import AddTodoModal from "./AddTodoModal";
 import TodoCard from "./TodoCard";
+import TodoFilter from "./TodoFilter";
 
 const TodoContainer = () => {
     return (
         <div>
             <div className="flex justify-between mb-5">
                 <AddTodoModal></AddTodoModal>
-                <Button>Filter</Button>
+                <TodoFilter></TodoFilter>
             </div>
 
             <div className="bg-primary-gradient w-full h-full rounded-xl p-3 space-y-2">
