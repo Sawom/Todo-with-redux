15e95e6d030ddd56476203778a3428d98718a944
Sawom <asawom250@gmail.com>
2024-08-18 03:18:37 +0600

    update modal

--- src/components/todo/AddTodoModal.tsx
+++ src/components/todo/AddTodoModal.tsx
@@ -7,28 +7,42 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
+import { FormEvent, useState } from "react";
 import { Button } from "../ui/button";
 import { Input } from "../ui/input";
 import { Label } from "../ui/label";
+import { DialogClose } from "@radix-ui/react-dialog";
 
 const AddTodoModal = () => {
-  
+  const [task, setTask] = useState("");
+  const [description, setDescription] = useState("");
+
+  const onSubmit = (e: FormEvent) => {
+    e.preventDefault();
+
+    console.log({ task, description });
+  };
+
   return (
     <Dialog>
       <DialogTrigger asChild>
         <Button className="bg-primary-gradient font-semibold">Add Todo</Button>
       </DialogTrigger>
+
       <DialogContent className="sm:max-w-[425px]">
         <DialogHeader>
           <DialogTitle>Edit profile</DialogTitle>
           <DialogDescription>Add your task</DialogDescription>
         </DialogHeader>
-        <div className="grid gap-4 py-4">
+
+        {/* form */}
+        <form onSubmit={onSubmit} className="grid gap-4 py-4">
           <div className="grid grid-cols-4 items-center gap-4">
             <Label htmlFor="task" className="text-right">
               Task
             </Label>
             <Input
+              onBlur={(e) => setTask(e.target.value)}
               id="task"
               defaultValue="Pedro Duarte"
               className="col-span-3"
@@ -39,15 +53,18 @@ const AddTodoModal = () => {
               Description
             </Label>
             <Input
+              onBlur={(e) => setDescription(e.target.value)}
               id="description"
               defaultValue="@peduarte"
               className="col-span-3"
             />
           </div>
-        </div>
-        <DialogFooter>
-          <Button type="submit">Save changes</Button>
-        </DialogFooter>
+          <div className="flex justify-end">
+            <DialogClose asChild>
+                <Button type="submit">Save changes</Button>
+            </DialogClose>
+          </div>
+        </form>
       </DialogContent>
     </Dialog>
   );
